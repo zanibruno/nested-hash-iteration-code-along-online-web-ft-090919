548c841584c89351ea004546ac8c342b0a23f68a
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 15:27:52 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |person , details|
+  details.each do |key, value| 
+  if key == :favorite_ice_cream_flavors
+    value.delete_if {|flavour| flavour = "strawberry"}
 end
+end
+end 
+end
+
